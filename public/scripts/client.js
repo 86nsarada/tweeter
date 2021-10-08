7b36919a6bb54f06f85a29ffff9f29873f80c3a0
Sarada <sarada@ranjits-mbp.lan>
2021-10-08 00:02:24 -0400

    ajax server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(()=>{
+   const createTweetElement =(tweetData)=>{
+var $user = $("h3").text(tweetData.user.name);
+const $
+} 
 
+})
