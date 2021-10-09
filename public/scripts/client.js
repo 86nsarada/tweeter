2a9712e6827c21a7e8e868a46306467252ed0bb8
Sarada <sarada@ranjits-mbp.lan>
2021-10-08 23:22:31 -0400

    done form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,10 +3,151 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(()=>{
-   const createTweetElement =(tweetData)=>{
-var $user = $("h3").text(tweetData.user.name);
-const $
-} 
+// Fake data taken from initial-tweets.json
 
+
+jQuery(document).ready(function () {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+
+  function loadTweets() {
+    console.log("inside load tweets");
+    $.ajax({
+        url: '/tweets',
+        method: 'GET',
+        datatype: "json",
+        success: (tweets) => {
+            //console.log("data:", tweets);
+            tweets.reverse();
+            renderTweets(tweets);
+        }
+    })
+}
+loadTweets();
+$("#error").hide();
+
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    
+    //$('#tweet-container').empty();
+    console.log("render tweets is running");
+    for (let tweet of tweets) {
+      //alert("is this thing really really on?");
+      jQuery('#tweet-container').append(createTweetElement(tweet));
+      // const $tweet = createTweetElement(tweet);      
+      //  $("#tweet-container").prepend($tweet); 
+    }
+  }
+  const createTweetElement = function (tweet) {
+    //let $tweet = /* Your code for creating the tweet element */
+    // ...
+    let markup = `
+         <article class="create-tweet">
+            <div class="photo">
+              <img class="profile" src="${tweet.user.avatars}">
+              
+              <span>${tweet.user.name}</span>
+            </div>
+            <div class="name">
+              <p>${tweet.user.handle}</p>
+            </div>
+          <div class="message">
+            <p>${tweet.content.text}</p>
+          </div>
+          <footer>
+            <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+          </footer>
+        </article>`;
+    return $(markup);
+    //return $("<h1>Hello world<h1>");
+    //<span>${timeago.format(tweet.created_at)}</span>
+
+  };
+
+  
+  renderTweets(data);
+  console.log("hello");
+
+
+/* <article class="create-tweet">
+          <div class="photo">
+            <img src="${tweet.user.avatars}">
+            <span>${tweet.user.name}</span>
+          </div>
+          <div class="name">
+            <p>${tweet.user.handle}</p>
+          </div>
+          <div class="message">
+            <p>${escape(tweet.content.text)}</p>
+          </div>
+          <!--<label for ="user-tweet-text">user 1 tweet!</label>
+
+          <textarea name="new-text" id="new-tweet-text"></textarea>-->
+
+          <footer>
+            <span>${jquery.timeago(tweet.created_at)}</span>
+            <div class="icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+
+
+            </div>
+
+          </footer
+        </article> */
+        /* tweet form submission */
+    $("#new-tweet-form").submit(function (event) {
+      console.log('event');
+      event.preventDefault();
+      let charCount = $("#tweet-text").val().length;
+      if (charCount === 0) {
+        console.log("my hello world")
+          $(".displayerror").text("You need letters to tweet");
+          $("#error").slideDown();
+      } else if (charCount > 140){ 
+        alert("more than 140 characters...please tweet upto 140 characters!");
+          $(".displayerror").text("Too many words to tweet");
+          $("#error").slideDown();
+      } else {
+          console.log($( this ).serialize());
+          const serializedData = $(this).serialize();
+          $.post("/tweets", serializedData, (response) => {
+              loadTweets();
+              $("#tweet-text").val("");
+          });
+      }
+      console.log("hello world here?")
+  })
 })
